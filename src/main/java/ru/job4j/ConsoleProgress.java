90774bd94ceea1b9dde343b9d596cb2c623ddd05
ListerioAxxe <langua095@gmail.com>
2021-08-18 16:56:44 +0300

    Консоль прогресс

---
+++ src/main/java/ru/job4j/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new ConsoleProgress());
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        String[] process = {"\\", "|", "/", "-", "\\", "\\", "|", "/", "-", "\\"};
+        for (int i = 0; i < process.length; i++) {
+            if (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r load: " + process[i]);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
