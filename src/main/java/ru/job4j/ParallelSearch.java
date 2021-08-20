69a1ab88b3d02fc4eda479d722ed607fb8c801cd
ListerioAxxe <langua095@gmail.com>
2021-08-20 15:42:42 +0300

    ParallelSearch

---
+++ src/main/java/ru/job4j/ParallelSearch.java
@@ -0,0 +1,42 @@
+package ru.job4j;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread sec = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        sec.start();
+        consumer.join();
+        sec.join();
+        consumer.interrupt();
+
+    }
+}
