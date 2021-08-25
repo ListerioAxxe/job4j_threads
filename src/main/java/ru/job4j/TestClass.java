44db6c6b891b4341f0a7ebd7b7c8ab758e0594b1
ListerioAxxe <langua095@gmail.com>
2021-08-25 12:28:10 +0300

    ForkJoinPoolExample

--- src/main/java/ru/job4j/TestClass.java
+++ src/main/java/ru/job4j/TestClass.java
@@ -1,8 +1,18 @@
 package ru.job4j;
 
-public class TestClass {
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestClass extends Thread {
     public static void main(String[] args) {
-        int id = Runtime.getRuntime().availableProcessors();
-        System.out.println(id);
+        byte a  = 127;
+        a++;
+        System.out.println(a);
+     }
     }
-}
